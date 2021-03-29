bd92960d31efe1d60339ee2115d82aea8523c2cd
Caroline <carolinebirkinshaw@outlook.com>
2021-03-29 19:09:53 +0100

    set up repository and add html and script files for task 1

---
+++ scripts/task-1.js
@@ -0,0 +1,6 @@
+function task1() {
+    var task1Image = document.getElementsByTagName('img')[0];
+    task1Image.src = "http://placekitten.com/g/200/200";
+    var task1Header = document.getElementById('task1-header');
+    task1Header.style.color = 'red';
+  } 
